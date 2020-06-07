2c292401a73000f82b2e8f8c61f448a6d9ced7c2
Fernando Elger <fee.elger@gmail.com>
2020-06-07 01:30:42 -0300

    testes integracao somador

---
+++ src/test/java/verival/SomadorTest.java
@@ -0,0 +1,42 @@
+package verival;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SomadorTest {
+    Somador somador;
+    Numero num;
+    Numero outro;
+
+
+    @BeforeEach
+    public void setup() {
+        somador = new Somador();
+    }
+
+    @Test
+    public void sumTest() {
+        num = new Numero(5);
+        outro = new Numero(3);
+
+        somador.set(num);
+        somador.mais(outro);
+
+        int expected = 8;
+        assertEquals(expected, somador.resultado().valor());
+    }
+
+    @Test
+    public void subtractionTest() {
+        num = new Numero(20);
+        outro = new Numero(10);
+
+        somador.set(num);
+        somador.menos(outro);
+
+        int expected = 10;
+        assertEquals(expected, somador.resultado().valor());
+    }
+}
